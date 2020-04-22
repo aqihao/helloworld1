b0209c470fd594ad19261143c1bcd996b39f1638
aqihao <4305958@qq.com>
2020-04-22 16:35:01 +0800

    测试

---
+++ src/test/Sttt.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @author deng
+ * @Description: 测试
+ * @date 2020/4/22 16:32
+ */
+public class Sttt {
+    public static void main(String[] args) {
+        System.out.println("9999");
+    }
+
+}
