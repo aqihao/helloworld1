d235217896d209f09e37fd8bb79359eb41cf0bde
aqihao <4305958@qq.com>
2020-04-22 15:54:35 +0800

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author deng
+ * @Description: ${todo}(这里用一句话描述这个类的作用)
+ * @date 2020/4/22 14:54
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
